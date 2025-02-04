2b95dd01f2fa9d0ede73a6d7a29c85493574667b
Jorge Noa <jorgent8607@gmail.com>
2025-02-04 20:52:26 +0100

    Initial commit

---
+++ src/app/calculator/calculator.service.ts
@@ -0,0 +1,5 @@
+export class CalculatorService{
+sum(a:number,b:number){
+return a+b+1;
+}
+}
---
+++ src/test/calculator/calculator.service.test.ts
@@ -0,0 +1,21 @@
+import { CalculatorService } from "../../app/calculator/calculator.service";
+
+describe('Calculator Service test suite',()=>{
+    
+    it('should return a sum if arguments are numbers',()=>{
+        
+        //arrange
+
+        const a=5;
+        const b=10;
+        const calcuatorService = new CalculatorService();
+        
+        //act
+
+        const act = calcuatorService.sum(a,b)
+        
+        //assert
+
+        expect(act).toEqual(16)
+    })
+})
