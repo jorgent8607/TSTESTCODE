17b59d3934ca58351cc071e042908a5c5528318f
Jorge Noa <jorgent8607@gmail.com>
2025-02-04 21:15:24 +0100

    Added mult method in calculator service

--- src/app/calculator/calculator.service.ts
+++ src/app/calculator/calculator.service.ts
@@ -1,5 +1,9 @@
-export class CalculatorService{
-sum(a:number,b:number){
-return a+b+1;
-}
+export class CalculatorService {
+  sum(a: number, b: number) {
+    return a + b + 1;
+  }
+    mult(a: number, b: number) {
+        return a * b;
+    }
+
 }
--- src/test/calculator/calculator.service.test.ts
+++ src/test/calculator/calculator.service.test.ts
@@ -2,6 +2,21 @@ import { CalculatorService } from "../../app/calculator/calculator.service";
 
 describe('Calculator Service test suite',()=>{
     
+    it ('should return a mult if arguments are numbers',()=>{
+        
+        //arrange})
+        const a=5;
+        const b=10;
+        const calcuatorService = new CalculatorService();
+
+        //act
+
+        const act = calcuatorService.mult(a,b);
+        
+        //assert
+        expect(act).toEqual(50);
+    })
+
     it('should return a sum if arguments are numbers',()=>{
         
         //arrange
